d849cf69b4ee4612ef77fbe42ab85bd7b3cd0ed8
AliAngel564 <jonathan.aliangel@gmail.com>
2026-05-15 10:06:28 -0600

    🚧 WIP
    Movimiento y esas cositas.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,19 +7,31 @@ public class PlayerController : MonoBehaviour
 {
     private InputSystem_Actions actions;
     private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private bool isGrounded = false;
     
     private Rigidbody2D rb;
     private Vector2 moveDirection;
 
     [Header("Visual Components")] 
     [SerializeField] private SpriteRenderer spriteRenderer;
-    [Header("Movement Variables")]
+
+    [Header("Movement Variables")] 
+    [SerializeField] private float startingSpeed;
+    [SerializeField]private float maxSpeed = 10f;
     [SerializeField]private float moveSpeed;
+    [SerializeField] private float jumpStrength;
+    private float pastSpeed;
+    
+    [Header("Debug I Guess")]
+    [SerializeField]private ContactPoint2D[] contactPoints;
 
     private void Awake()
     {
         actions = new InputSystem_Actions();
         rb = GetComponent<Rigidbody2D>();
+        actions.Player.Jump.started += OnJump; 
     }
 
     private void OnEnable()
@@ -38,16 +51,33 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        pastSpeed = rb.linearVelocityX;
+        ManageMovement();
+    }
+    
     void Update()
     {
-        ManageMovement();
         ManageVisualAspect();
+        Debug.Log("Is Grounded: "+isGrounded);
     }
 
     void ManageMovement()
     {
-        moveDirection = moveAction.ReadValue<Vector2>().normalized;
-        rb.AddForce(moveDirection * moveSpeed,ForceMode2D.Force);
+        
+       
+        
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up*jumpStrength, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
     }
 
     void ManageVisualAspect()
@@ -60,4 +90,12 @@ public class PlayerController : MonoBehaviour
             spriteRenderer.flipX = false;
         }
     }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        isGrounded = true;
+        contactPoints = new ContactPoint2D[other.contactCount];
+        other.GetContacts(contactPoints);
+    }
+    
 }
